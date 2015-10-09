6d39cc2aa19366f5cdd6b38a6f172b52cb3d84a5
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-10-09 11:30:14 -0400

    Add client API shared specs for Ruby adapters
    Any Ruby adapter can behaviour-drive it implementation of
    the Bottled Water client internal API by using these shared specs.
    
    Example:
    
        # spec/lib/your_ruby_client_spec.rb
    
        describe 'Bottled Water Ruby client' do
    
          before(:each) { @subject = YourRubyClient.new }
    
          it_behaves_like 'a Bottled Water for PostgreSQL client'
        end

--- ruby/spec/support/spec_for_bottled_water_pg_client.rb
+++ ruby/spec/support/spec_for_bottled_water_pg_client.rb
@@ -1,8 +1,15 @@
 RSpec.shared_examples 'a Bottled Water for PostgreSQL client' do
 
   BOTTLED_WATER_PG_API = [
-    :some_api_function_placeholder,
-    :another_api_function_placeholder # replace these placeholders by the actual function names
+    :bw_client_context_new,
+    :bw_client_context_free,
+    :bw_set_begin_txn_callback,
+    :bw_set_on_commit_txn_callback,
+    :bw_set_on_table_schema_callback,
+    :bw_set_on_insert_row_callback,
+    :bw_set_on_update_row_callback,
+    :bw_set_on_delete_row_callback,
+    :bw_run#, :bw_stop (not yet implemented in Bottled Water)
   ]
 
   describe 'it implements the Bottles Water client API', public: true do
